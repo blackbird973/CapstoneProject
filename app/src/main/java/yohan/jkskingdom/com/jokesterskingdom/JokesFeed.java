f584d011508e4d9b120f525aed0a5a3af94c3031
blackbird973 <anthogamix@protonmail.com>
2018-06-29 23:25:14 +0200

    Add Logout Feature, add the bottom navigation bar, and managed to implement the fragments

--- app/src/main/java/yohan/jkskingdom/com/jokesterskingdom/JokesFeed.java
+++ app/src/main/java/yohan/jkskingdom/com/jokesterskingdom/JokesFeed.java
@@ -2,8 +2,13 @@ package yohan.jkskingdom.com.jokesterskingdom;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,10 +19,70 @@ import android.widget.TextView;
 
 public class JokesFeed extends AppCompatActivity {
 
+    private FloatingActionButton fabAddJoke;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jokesfeed);
 
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        fabAddJoke = findViewById(R.id.floatingActionButton);
+
+
+
+
+        //AFFICHE LE FRAGMENT DU JOKES FEED DES LE DEBUT
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new JokesFeedFragment()).commit();
+
+        //START FRAGMENT ON CLICK OF THE FAB
+        fabAddJoke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AddJokeFragment()).commit();
+            }
+        });
+
+
+
+
+
     }
+    //AFFICHE LES FRAGMENT CORRESPONDANT A CHAQUE CLIQUE SUR DES ITEM DE LA NAVBAR
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                        case R.id.nav_feed:
+                            selectedFragment = new JokesFeedFragment();
+                            break;
+
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+
+                }
+            };
+
+
+
+
+
+
+
+
+
 }
